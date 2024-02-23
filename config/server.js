14b312fa66cacdd995092c2dbc76c5eef1377625
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-02-23 12:59:51 +0300

    asdsa

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e0a3c4731adeb40c360d881e0dee3a8c'),
